0e8c00539eabe53137413e20694b01d04db294b7
pragun0910-pixel <pragun0910@gmail.com>
2026-03-03 08:48:53 +0530

    joining strings

--- oopsbanner.java
+++ oopsbanner.java
@@ -1,11 +1,28 @@
-public class Main {
+public class OOPSBannerAppUC3 {
+
     public static void main(String[] args) {
 
-        System.out.println(" *****   *****   ******   ***** ");
-        System.out.println("*     * *     * *     * *      ");
-        System.out.println("*     * *     * ******   ***** ");
-        System.out.println("*     * *     * *            * ");
-        System.out.println(" *****   *****  *       *****  ");
+        // Printing OOPS Banner using String.join()
 
+        System.out.println(String.join(" ", 
+                "  *  ", "  **  ", " **  "));
+
+        System.out.println(String.join(" ", 
+                " *  * ", " *   * ", " *   * "));
+
+        System.out.println(String.join(" ", 
+                "*    *", "*    *", "*    *"));
+
+        System.out.println(String.join(" ", 
+                "*    *", "*    *", " ***  "));
+
+        System.out.println(String.join(" ", 
+                "*    *", "*    *", "    **   "));
+
+        System.out.println(String.join(" ", 
+                " *  * ", " *   * ", " *  *  "));
+
+        System.out.println(String.join(" ", 
+                "  *  ", "  **  ", " **  "));
     }
 }
